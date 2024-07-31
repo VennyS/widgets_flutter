c20438cc4dbad443306331386c92a7f72b148a1c
Danil Panarin <panarin.danil.08@gmail.com>
2024-07-31 21:22:43 +0500

    Добавил assets и обновил main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'custom_tag.dart';
 import 'custom_button.dart';
+import 'universal_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,102 +10,81 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF006FFD)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Виджеты'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomButton(
-              variant: CustomButtonVariants.primary,
-              text: "Click me",
-              // showLeftIcon: true,
-              // showRightIcon: true,
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            CustomTag(
-              text: "Tag",
-            )
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 8),
+              UniversalButton(
+                id: 12,
+                title: "Заголовок, максимум 2 строк",
+                desc: "Описание краткое, максимум 3-4 строчки",
+                state: UniversalButtonState.skeleton,
+                onPressed: () => print("Skeleton click"),
+              ),
+              const SizedBox(height: 24),
+              UniversalButton(
+                id: 12,
+                title: "Заголовок, максимум 2 строк",
+                desc: "Описание краткое, максимум 3-4 строчки",
+                state: UniversalButtonState.active,
+                onPressed: () => print("Active click"),
+              ),
+              const SizedBox(height: 24),
+              UniversalButton(
+                id: 12,
+                title: "Заголовок, максимум 2 строк",
+                desc: "Описание краткое, максимум 3-4 строчки",
+                state: UniversalButtonState.highlight,
+                onPressed: () => print("Highlight click"),
+              ),
+              const SizedBox(height: 24),
+              const CustomButton(
+                variant: CustomButtonVariants.primary,
+                text: "Click me",
+              ),
+              const SizedBox(height: 24),
+              const CustomButton(
+                variant: CustomButtonVariants.secondary,
+                text: "Click me",
+              ),
+              const SizedBox(height: 24),
+              const CustomButton(
+                variant: CustomButtonVariants.terciary,
+                text: "Click me",
+              ),
+              const SizedBox(height: 24),
+              const CustomTag(text: "Tag"),
+              const SizedBox(height: 24),
+            ],
+          ),
         ),
       ),
     );
